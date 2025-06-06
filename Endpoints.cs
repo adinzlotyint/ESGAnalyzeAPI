583298067f093e4645af185396e3b83c60c8b571
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-06-06 10:34:33 +0200

    Fix: Add using

--- Endpoints.cs
+++ Endpoints.cs
@@ -1,4 +1,7 @@
-﻿public static class Endpoints {
+﻿using ESGAnalyzeAPI.Models;
+using ESGAnalyzeAPI.Services;
+
+public static class Endpoints {
     public static void MapEndpoints(this IEndpointRouteBuilder app) {
         app.MapPost("/analyze/pdf", async (IFormFile file, IParseService parseService, IAnalyzer analyzer) => {
             if (file == null || Path.GetExtension(file.FileName)?.ToLower() != ".pdf")
