95768c72870b9e9e90be8487379c627870319e4f
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-06-06 11:26:12 +0200

    refactor: Adjust interface name

--- Endpoints.cs
+++ Endpoints.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public static class Endpoints {
     public static void MapEndpoints(this IEndpointRouteBuilder app) {
-        app.MapPost("/analyze/pdf", async (IFormFile file, [FromServices] IParseService parseService, [FromServices] IAnalyzer analyzer) => {
+        app.MapPost("/analyze/pdf", async (IFormFile file, [FromServices] IParseService parseService, [FromServices] IAnalyzerService analyzer) => {
             if (file == null || Path.GetExtension(file.FileName)?.ToLower() != ".pdf")
                 return Results.BadRequest("Only .pdf files are supported.");
 
