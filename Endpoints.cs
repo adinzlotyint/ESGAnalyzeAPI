18e564121c906aa3a5408137907cb47e081b7c3a
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-06-06 11:15:53 +0200

    feat: Add DisableAntiforgery

--- Endpoints.cs
+++ Endpoints.cs
@@ -1,15 +1,16 @@
 ﻿using ESGAnalyzeAPI.Models;
 using ESGAnalyzeAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 public static class Endpoints {
     public static void MapEndpoints(this IEndpointRouteBuilder app) {
-        app.MapPost("/analyze/pdf", async (IFormFile file, IParseService parseService, IAnalyzer analyzer) => {
+        app.MapPost("/analyze/pdf", async (IFormFile file, [FromServices] IParseService parseService, [FromServices] IAnalyzer analyzer) => {
             if (file == null || Path.GetExtension(file.FileName)?.ToLower() != ".pdf")
                 return Results.BadRequest("Only .pdf files are supported.");
 
             string text = await parseService.ExtractTextFromPDFAsync(file);
             ESGAnalysisResult result = analyzer.Analyze(text);
             return Results.Ok(result);
-        });
+        }).DisableAntiforgery(); ;
     }
 }
