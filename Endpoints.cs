ddd11b2d8930b8690ed021738f66117db38c0b67
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-06-06 15:23:31 +0200

    feat: Add logger

--- Endpoints.cs
+++ Endpoints.cs
@@ -4,13 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 
 public static class Endpoints {
     public static void MapEndpoints(this IEndpointRouteBuilder app) {
-        app.MapPost("/analyze/pdf", async (IFormFile file, [FromServices] IParseService parseService, [FromServices] IAnalyzerService analyzer) => {
-            if (file == null || Path.GetExtension(file.FileName)?.ToLower() != ".pdf")
-                return Results.BadRequest("Only .pdf files are supported.");
+        app.MapPost("/analyze/pdf", async (IFormFile file, [FromServices] IParseService parseService,
+                    [FromServices] IAnalyzerService analyzer, [FromServices] ILogger<Program> logger) => {
+                        if (file == null || Path.GetExtension(file.FileName)?.ToLower() != ".pdf") {
+                            logger.LogWarning("Invalid file uploaded: {FileName}", file?.FileName);
+                            return Results.BadRequest("Only .pdf files are supported.");
+                        }
 
-            string text = await parseService.ExtractTextFromPDFAsync(file);
-            ESGAnalysisResult result = analyzer.Analyze(text);
-            return Results.Ok(result);
-        }).DisableAntiforgery(); ;
+                        string text = await parseService.ExtractTextFromPDFAsync(file);
+                        ESGAnalysisResult result = analyzer.Analyze(text);
+                        return Results.Ok(result);
+                    }).DisableAntiforgery();
+
     }
 }
